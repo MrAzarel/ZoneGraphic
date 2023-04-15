8db54fe3e97f0e3e3e6baca692647154abc09078
MrAzarel <93323890+MrAzarel@users.noreply.github.com>
2023-04-16 02:12:59 +0300

    Add files via upload

---
+++ Assets/Polygon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Polygon
+{
+    public int Width;
+    public int Height;
+    decimal[,] Vertices;
+
+    public Polygon(int width, int height, decimal[,] vertices)
+    {
+        Width = width;
+        Height = height;
+        Vertices = vertices;
+    }
+}
+
---
+++ Assets/Spawn.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    //твои зоны
+    [SerializeField] public GameObject tile1;
+    public Polygon[] tiles = new Polygon[1];
+    decimal[,] vertices = new decimal[4, 2];
+
+    void Start()
+    {
+        Spawner();
+    }
+
+    public void Spawner()
+    {
+        //это просто для теста. Можешь удалить.
+        vertices[0, 0] = 0;
+        vertices[0, 1] = 2;
+
+        vertices[1, 0] = 2;
+        vertices[1, 1] = 2;
+
+        vertices[2, 0] = 2;
+        vertices[2, 1] = 0;
+
+        vertices[3, 0] = 0;
+        vertices[3, 1] = 0;
+
+
+        tiles[0] = new Polygon(2, 2, vertices);
+
+        //тут размещаем все зоны.
+        for (int i = 0; i < tiles.Length; i++)
+        {
+            tile1.transform.localScale = new Vector3(tiles[i].Width, tiles[i].Height);
+            tile1.transform.position = new Vector3((float)((vertices[0, 0] + vertices[1, 0]) / 2), (float)((vertices[1, 1] + vertices[2, 1]) / 2));
+        }
+    }
+}
