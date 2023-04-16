d049a41a8f1e6148769dfc4b0648543a35c2f7a3
MrAzarel <93323890+MrAzarel@users.noreply.github.com>
2023-04-16 03:31:15 +0300

    Add files via upload

--- Assets/Polygon.cs
+++ Assets/Polygon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Polygon : IPolygon
 {
+    public GameObject tile = GameObject.Find("Cube");
     public Polygon(decimal[,] vertices)
     {
         Vertices = vertices;
--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -1,20 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    //твои зоны
-    [SerializeField] public GameObject tile1;
+    //этот лист тебе не нужен. Потом передай сюда свой.
     public List<IPolygon> tiles = new List<IPolygon>();
+    //и это тоже тебе не надо
     decimal[,] vertices = new decimal[4, 2];
 
     void Start()
     {
-        Spawner();
+        //сюда в качестве аргумента передай свой лист
+        Spawner(tiles);
     }
 
-    public void Spawner()
+    public void Spawner(List<IPolygon> polygons)
     {
         //это просто для теста. Можешь удалить.
         vertices[0, 0] = 0;
@@ -33,10 +35,11 @@ public class Spawn : MonoBehaviour
         tiles.Add(new Polygon(vertices));
 
         //тут размещаем все зоны.
-        for (int i = 0; i < tiles.Count; i++)
+        for (int i = 0; i < polygons.Count; i++)
         {
-            tile1.transform.localScale = new Vector3((float)(tiles[i].Vertices[0, 0] + tiles[i].Vertices[1, 0]), (float)(tiles[i].Vertices[1, 1] + tiles[i].Vertices[2, 1]));
-            tile1.transform.position = new Vector3((float)((tiles[i].Vertices[0, 0] + tiles[i].Vertices[1, 0]) / 2), (float)((tiles[i].Vertices[1, 1] + tiles[i].Vertices[2, 1]) / 2));
+            ((Polygon)polygons[i]).tile.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0, 255) / 100, Random.Range(0, 255) / 100, Random.Range(0, 255) / 100, 1);
+            ((Polygon)polygons[i]).tile.transform.localScale = new Vector3((float)(polygons[i].Vertices[0, 0] + polygons[i].Vertices[1, 0]), (float)(polygons[i].Vertices[1, 1] + polygons[i].Vertices[2, 1]), 1);
+            ((Polygon)polygons[i]).tile.transform.position = new Vector3((float)((polygons[i].Vertices[0, 0] + polygons[i].Vertices[1, 0]) / 2), (float)((polygons[i].Vertices[1, 1] + polygons[i].Vertices[2, 1]) / 2));
         }
     }
 }
